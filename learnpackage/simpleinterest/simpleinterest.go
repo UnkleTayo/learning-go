a4231f5108e2f7e339fa6a57099dbb7866e155a8
Adetayo <akinsanyaadetayo7@gmail.com>
2020-12-23 09:39:03 +0100

    Created a simple Go package
    Signed-off-by: Adetayo <akinsanyaadetayo7@gmail.com>

---
+++ learnpackage/simpleinterest/simpleinterest.go
@@ -0,0 +1,14 @@
+package simpleinterest
+
+import "fmt"
+
+func init() {
+	fmt.Println("Simple interest calculation")
+
+}
+
+//Calculate calculates and returns the simple interest for a principal p, rate of interest r for time duration t years
+func Calculate(p float64, r float64, t float64) float64 {
+	interest := p * (r / 100) * t
+	return interest
+}
