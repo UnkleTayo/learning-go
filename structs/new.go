4e33c21a94227d19521b36d20c2ae2d26f3eab7f
Adetayo <akinsanyaadetayo7@gmail.com>
2020-12-24 14:36:46 +0100

    declared a map

--- structs/new.go
+++ structs/new.go
@@ -16,15 +16,6 @@ type person struct {
 
 
 func main() {
-	// alex := person{firstName: "Kakashi", lastName: "Hatake"}
-	// fmt.Println(alex)
-
-	
-// var bobby person
-// fmt.Println(bobby)
-
-// listing out the values inside the Go 
-// fmt.Printf("%+v", alex)
 
 // Embedding structs 
 	jim := person{
@@ -36,14 +27,22 @@ func main() {
 		},
 	}
 
+	// getting addrss in memory 
+	// jimPointer := &jim
+	// & is an operator that returns the memory adress of the value that the jim variable is pointing at 
+	// fmt.Println(jimPointer)
 	jim.updateName("jimmy")
 	jim.print()
 }
-func(p *person) updateName(newFirstName string){
-	p.firstName = newFirstName
+
+// * returns the value that the memory adderess is pointing at 
+// * in front of type means the updatename can only be called with reference to a pointer of that type 
+func(pointerToPerson *person) updateName(newFirstName string){
+	(*pointerToPerson).firstName = newFirstName
 }
 
-// receiver function 
+// receiver function  
+
 func (p person) print(){
 	fmt.Printf("%+v", p)
 }
