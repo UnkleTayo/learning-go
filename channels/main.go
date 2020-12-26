c1427d19a7fc46dbea222324550a2e640b6f4369
Adetayo <akinsanyaadetayo7@gmail.com>
2020-12-26 22:20:17 +0100

    into to channels

---
+++ channels/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+
+func main() {
+	links := []string{
+		"http://gmail.com",
+		"http://facebook.com",
+		"http://golang.org",
+		"http://amazon.com",
+		"http://twitter.com",
+	}
+
+	// creating channel
+	 c := make(chan string)
+
+	for _, link := range links {
+	go checkLink(link, c)
+}
+
+for l := range c {
+	go func(link string){
+		time.Sleep(5 * time.Second)
+		checkLink(link, c)
+	}(l)
+}
+}
+
+
+func checkLink(link string , c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down")
+		c <- link
+		return
+	} 
+
+	fmt.Println(link, "is up!")
+	c <- link
+}
