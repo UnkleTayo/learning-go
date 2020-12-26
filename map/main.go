9fef141a0231a3e7e99b8423e4e921773b620472
Adetayo <akinsanyaadetayo7@gmail.com>
2020-12-26 22:20:45 +0100

    Channels intro
    Signed-off-by: Adetayo <akinsanyaadetayo7@gmail.com>

--- map/main.go
+++ map/main.go
@@ -4,10 +4,19 @@ import "fmt"
 
 func main() {
 	// declaring a map
+	// var colors map[string]string
+	// colors := make(map[string]string)
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4bf745",
+		"white": "#fff",
 	}
 
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex Code for", color, "is", hex)
+	}
 }
