c0e7e6842433c07981b64c912308123ecfaf586c
Adetayo <akinsanyaadetayo7@gmail.com>
2021-01-11 00:07:51 +0100

    Map revisited

--- map/Readme.md
+++ map/Readme.md
@@ -1,3 +1,30 @@
 # Map
 
+A map is a builtin type in Go that is used to store key-value pairs.
 maps are just like Go's representation of hash tables data structure, it allows you to map one arbitrary type to another.
+
+## How to create a Map
+
+A map can be created by passing the type of key and value to the make function. The following is the syntax to create a new map.
+
+```Go
+make(map[type of key]type of value)
+```
+
+E.g
+
+```Go
+playerSalary := make(map[string]int)
+```
+
+## Deleting items from a map
+
+delete(map, key) is the syntax to delete key from a map. The delete function does not return any value.
+
+## Map of struct
+
+So far we have only been storing the salary of the employee in the map. Wouldn't it be nice if we are able to store the country of each employee in the map too? This can be achieved by using a map of structs. The employee can be represented as a struct containing fields salary and country and they will be stored in the map with a string key and struct value. Let's write a program to understand how this can be done.
+
+## Length of the map
+
+Length of the map can be determined using the len function.
--- map/main.go
+++ map/main.go
@@ -2,21 +2,103 @@ package main
 
 import "fmt"
 
+type employee struct {  
+	salary  int
+	country string
+}
+
 func main() {
 	// declaring a map
 	// var colors map[string]string
 	// colors := make(map[string]string)
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-		"white": "#fff",
-	}
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// 	"white": "#fff",
+	// }
 
-	printMap(colors)
+	// printMap(colors)
+	// printSalary()
+	// employeeStruct()
+	mapReference()
 }
 
 func printMap(c map[string]string) {
 	for color, hex := range c {
 		fmt.Println("Hex Code for", color, "is", hex)
 	}
+}
+
+func printSalary() {  
+	employeeSalary := make(map[string]int)
+	employeeSalary["steve"] = 12000
+	employeeSalary["jamie"] = 15000
+	employeeSalary["mike"] = 9000
+
+	// Retrieving value for a key from a map
+	employee:= "mike"
+	salary := employeeSalary[employee]
+	fmt.Println("Salary of", employee, "is", salary)
+
+
+	// checking od a key is present 
+	newEmp := "joe"
+	value, ok := employeeSalary[newEmp]
+
+	if ok == true {
+		fmt.Println("Salary of", newEmp, "is", value)
+		return
+		}
+		fmt.Println(newEmp, "not found")
+
+		fmt.Println("content of map")
+
+		// Iterate over all elements in a map
+		for key, value := range employeeSalary{
+			fmt.Printf("employeeSalary[%s] = %d\n", key, value)
+		}
+
+		// Deleting items from a map
+		fmt.Println("map before deletion", employeeSalary)
+    delete(employeeSalary, "steve")
+    fmt.Println("map after deletion", employeeSalary)
+}
+
+
+func employeeStruct(){
+			emp1 := employee{
+				salary:  12000,
+				country: "USA",
+		}
+		emp2 := employee{
+				salary:  14000,
+				country: "Canada",
+		}
+		emp3 := employee{
+				salary:  13000,
+				country: "India",
+		}
+
+		employeeInfo:= map[string]employee{
+			"Steve": emp1,
+			"Jamie": emp2,
+			"Mike": emp3,
+		}
+
+		for name, info := range employeeInfo {
+			fmt.Printf("Employee: %s Salary:$%d  Country: %s\n", name, info.salary, info.country)
+		}
+
+}
+
+func mapReference(){
+	employeeSalary := map[string]int{
+		"steve": 12000,
+		"jamie": 15000,     
+		"mike": 9000,
+}
+fmt.Println("Original employee salary", employeeSalary)
+modified := employeeSalary
+modified["mike"] = 18000
+fmt.Println("Employee salary changed", employeeSalary)
 }
