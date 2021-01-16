34b0114de3a3147d679af1a9405113587da70b26
Adetayo <akinsanyaadetayo7@gmail.com>
2021-01-16 19:56:15 +0100

    Learnt about methods

--- methods/Readme.md
+++ methods/Readme.md
@@ -1,4 +1,11 @@
-# Methods
+# Method
+
+A method is just a function with a special receiver type between the func keyword and the method name. The receiver can either be a struct type or non-struct type.
+
+```Go
+func (t Type) methodName(parameter list) {
+}
+```
 
 ```Go
 Note - One thing you should note when working with methods is that, like functions, they create copies of the arguments passed into it. To avoid this, we can use pointer receivers when defining our methods
--- methods/main.go
+++ methods/main.go
@@ -4,22 +4,41 @@ import "fmt"
 
 // This comment should make it stop screeming
 type Employee struct {
-	Name string
+	name string
+	age int
 }
 
-// create a reference function
-func (e *Employee) UpdateName(newName string) {
-	e.Name = newName
-}
+// creating type alias 
+type myInt int
 
-// create a reference function
-func (e *Employee) PrintName() {
-	fmt.Println(e.Name)
+
+// Method with value receiver  
+func (e Employee) changeName(newName string) {  
+	e.name = newName
+}
+// Method with pointer receiver  
+func (e *Employee) changeAge(newAge int) {  
+	e.age = newAge
 }
 
 func main() {
-	var employee Employee
-	employee.Name = "Elliot"
-	employee.UpdateName("Forbsey")
-	employee.PrintName()
+	e := Employee{
+		name: "Mark Andrew",
+		age:  50,
+}
+
+	p := &e
+	fmt.Printf("Type of %x\n", p)
+	
+	num1 := myInt(5)
+    num2 := myInt(10)
+    sum := num1.add(num2)
+    fmt.Println("Sum is", sum)
+}
+
+
+// DEFINING METHID ON STRYCT TYPE 
+// In the program above, in line no. 3 we are trying to add a method named add on the built-in type int. This is not allowed since the definition of the method add and the definition of type int is not in the same package.
+func (a myInt) add(b myInt) myInt {
+	return a + b  
 }
