63d31cb3b68f62d65fca1dbca55f9c476ac9ced8
Adetayo <akinsanyaadetayo7@gmail.com>
2021-03-10 03:28:06 +0100

    Create a simple server

--- server/main.go
+++ server/main.go
@@ -2,22 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Println(writer, "Hello World")
-	})
+const (
+	connHost = "localhost"
+	connPort = "8080"
+)
 
-	server := http.Server{
-		Addr: "9001",
-	}
-	fmt.Println("starting server")
-
-	if err := server.ListenAndServe(); err != nil {
-		panic(err)
-	}
-
-	Handler()
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world")
 }
+
+// handling routes
+func main() {
+	http.HandleFunc("/", helloWorld)
+	err := http.ListenAndServe(connHost+":"+connPort, nil)
+	if err != nil {
+		log.Fatal("error starting http server", err)
+		return
+	}
+}
+
+// func main() {
+// http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+// 	fmt.Println(writer, "Hello World")
+// })
+
+// server := http.Server{
+// 	Addr: "9001",
+// }
+// fmt.Println("starting server")
+
+// if err := server.ListenAndServe(); err != nil {
+// 	panic(err)
+// }
+
+// Handler()
+// Handling()
+// }
---
+++ server/simple-server.go
@@ -0,0 +1,26 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// )
+
+// const (
+// 	connHost = "localhost"
+// 	connPort = "8080"
+// )
+
+// func helloWorld(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello world")
+// }
+
+// // handling routes
+// func Handling() {
+// 	http.HandleFunc("/", helloWorld)
+// 	err := http.ListenAndServe(connHost+":"+connPort, nil)
+// 	if err != nil {
+// 		log.Fatal("error starting http server", err)
+// 		return
+// 	}
+// }
