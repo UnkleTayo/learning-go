c468c87a7c3ac163c10f442d0f4b231899f9d358
Adetayo <akinsanyaadetayo7@gmail.com>
2021-03-10 04:07:48 +0100

    created a protected route

--- server/main.go
+++ server/main.go
@@ -1,44 +1,19 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-)
-
-const (
-	connHost = "localhost"
-	connPort = "8080"
-)
-
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world")
-}
-
-// handling routes
 func main() {
-	http.HandleFunc("/", helloWorld)
-	err := http.ListenAndServe(connHost+":"+connPort, nil)
-	if err != nil {
-		log.Fatal("error starting http server", err)
-		return
-	}
+	// http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	// 	fmt.Println(writer, "Hello World")
+	// })
+
+	// server := http.Server{
+	// 	Addr: "9001",
+	// }
+	// fmt.Println("starting server")
+
+	// if err := server.ListenAndServe(); err != nil {
+	// 	panic(err)
+	// }
+
+	// Handler()
+
 }
-
-// func main() {
-// http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-// 	fmt.Println(writer, "Hello World")
-// })
-
-// server := http.Server{
-// 	Addr: "9001",
-// }
-// fmt.Println("starting server")
-
-// if err := server.ListenAndServe(); err != nil {
-// 	panic(err)
-// }
-
-// Handler()
-// Handling()
-// }
--- server/simple-server.go
+++ server/simple-server.go
@@ -1,26 +1,48 @@
-// package main
+package main
 
-// import (
-// 	"fmt"
-// 	"log"
-// 	"net/http"
-// )
+import (
+	"crypto/subtle"
+	"fmt"
+	"log"
+	"net/http"
+)
 
-// const (
-// 	connHost = "localhost"
-// 	connPort = "8080"
-// )
+const (
+	connHost      = "localhost"
+	connPort      = "8080"
+	adminUser     = "admin"
+	adminPassword = "admin"
+)
 
-// func helloWorld(w http.ResponseWriter, r *http.Request) {
-// 	fmt.Fprintf(w, "Hello world")
-// }
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world")
+}
 
-// // handling routes
-// func Handling() {
-// 	http.HandleFunc("/", helloWorld)
-// 	err := http.ListenAndServe(connHost+":"+connPort, nil)
-// 	if err != nil {
-// 		log.Fatal("error starting http server", err)
-// 		return
-// 	}
-// }
+// BasicAuth is commented
+func BasicAuth(handler http.HandlerFunc, realm string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user, pass, ok := r.BasicAuth()
+		if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(adminUser)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(adminPassword)) != 1 {
+			w.Header().Set("WWW-Authenticate", `Basic real,="`+realm+`"`)
+			w.WriteHeader(401)
+			w.Write([]byte("You are not authorized to access the application"))
+			return
+		}
+		handler(w, r)
+	}
+}
+
+// handling routes
+func simpleServer() {
+	http.HandleFunc("/", BasicAuth(helloWorld, "Please enter your username and password"))
+	err := http.ListenAndServe(connHost+":"+connPort, nil)
+	if err != nil {
+		log.Fatal("error starting http server", err)
+		return
+	}
+}
+
+// JustRun is to be exported
+func JustRun() {
+	simpleServer()
+}
